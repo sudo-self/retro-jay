ff2b6af1248758ea46b7eb2cea954d3f50e9aa99
Sudo-Self <sudo-self@proton.me>
2024-02-02 14:36:23 -0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+          <div className="navbar bg-base-300">
+            <button className="btn btn-ghost text-xl">JesseJesse</button>
+          </div>
+    </>
+  )
+}
+
