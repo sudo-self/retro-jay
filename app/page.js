23ed5b13d48103d513e04a8bfdc16be64834b6b0
Sudo-Self <sudo-self@proton.me>
2024-02-02 20:10:49 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,54 @@ export default function Home() {
   return (
     <>
           <div className="navbar bg-base-300">
-            <button className="btn btn-ghost text-xl">JesseJesse</button>
+            <button className="btn btn-ghost text-xl">Retro.JesseJesse.com</button>
           </div>
-    </>
+          <div className="mockup-phone border-primary">
+            <div className="camera"></div>
+            <div className="display">
+              <div className="artboard artboard-demo phone-1"><iframe src="https://retro.jessejesse.com" name="myiFrame" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="600px" width="350px" allowfullscreen></iframe>🌼</div>
+            </div>
+          
+          </div>&nbsp;&nbsp;
+          <textarea rows="10" cols="30" className="textarea textarea-secondary" placeholder="daisyUI" readonly>
+Retro.JesseJesse.com all the games games are packed online and rendered directly in the browser. On chrome you can even pair your own library. from Gdrive. Full touchscreen support this is probably the best choice for everyday retro gaming. 100% free and with very minimal steps you can jump right into the action. Add a bluetooth controller for the best experience.
+          </textarea>
+          
+    
+         
+
+          <div className="avatar">
+            <div className="w-24 rounded">
+              <img src="https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/Cash Dark.gif" />
+            </div>
+          </div>
+        
+          <textarea rows="10" cols="30" className="textarea textarea-secondary" placeholder="daisyUI" readonly>
+Custom builds. these are single titles built ready to play. Easy one button launch is all that stands in the way of the SNES classic.
+          
+    punchout.jessejesse.com
+          
+          while retro assembly is great single game builds are perfect if your more serious about a specific title. and these builds address the memory issues with large game files from that era.
+          </textarea>
+          <div className="avatar">
+            <div className="w-24 rounded">
+              <img src="https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/Cash Light.gif" />
+            </div>
+          </div>
+          <div className="mockup-phone border-primary">
+            <div className="camera"></div>
+            <div className="display">
+              <div className="artboard artboard-demo phone-1"><iframe src="https://punchout.jessejesse.com" name="myiFrame" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="600px" width="350px" allowfullscreen></iframe>🌼</div>
+            </div>
+          </div>
+          <iframe src="https://play.jessejesse.com" scrolling="yes" frameborder="0" height="500px" width="600px"></iframe><iframe src="https://server.jessejesse.com" scrolling="yes" frameborder="0" height="500px" width="600px"></iframe>
+          
+          </>
+          
+          
   )
 }
+
+
+
 
