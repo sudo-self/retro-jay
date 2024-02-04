c0f3a84dd9c676c03ef0a01df207273bf4d748fb
Sudo-Self <sudo-self@proton.me>
2024-02-03 20:30:46 -0700

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <>
-        <div className="navbar bg-slate-900">
-        <button className="btn btn-outline relative px-6 py-3 transition-all ease-out bg-slate-900 rounded-md group-hover:bg-opacity-0 duration-400  text-xl text-white"><a href="https://retro-jay.vercel.app">SNES</a></button> 
+          <div className="navbar justify-center items-center bg-slate-900">
+        <button className="btn btn-info bg-slate-900 btn-outline text-white"><a href="https://retro-jay.vercel.app">Reset</a></button>
         &nbsp;&nbsp;<button class="btn btn-info bg-slate-900 btn-outline text-white"><a href="https://sms.jessejesse.com">smsCHAT</a></button>&nbsp;&nbsp;
-        <button class="btn btn-outline bg-slate-900 btn-outline text-white"><a href="https://retro.jessejesse.com">altServer</a></button>
+        <button class="btn btn-info bg-slate-900 btn-outline text-white"><a href="https://retro.jessejesse.com">altServer</a></button>
 </div>
 
-           <iframe src="https://play.jessejesse.com" height="500px" width="500px"></iframe>
-           <iframe src="https://server.jessejesse.com" height="500px" width="500px"></iframe>
+           <center><iframe src="https://play.jessejesse.com" height="500px" width="430px"></iframe>
+           <iframe src="https://server.jessejesse.com" height="500px" width="400px"></iframe></center>
 
 <div className="avatar">
   <div className="w-24 rounded">
@@ -21,5 +21,3 @@ export default function Home() {
     </>
   )
 }
-
-
